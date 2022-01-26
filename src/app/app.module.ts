e9322e1860a529b9f2d82aec0232c537cf4f43de
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2022-01-26 13:01:02 +0200

    routing issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule  } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+AuthService
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _AuthService : AuthService , private _Router: Router) { }
+  error:string ="";
+  loginForm = new FormGroup ({
+    email:new FormControl(null, [Validators.required,Validators.email]),
+    password:new FormControl(null, [Validators.required,Validators.pattern('^[A-Z][a-z0-9]{3,8}')])
+  })
 
   ngOnInit(): void {
   }
 
+  submitLoginForm(loginform:FormGroup){
+    this._AuthService.login(loginform.value).subscribe((response)=>{
+      if (response.message == "success") {
+        localStorage.setItem('userToken', response.token);
+        this._AuthService.saveCurrentUser();
+        this._AuthService.isLogin=true;
+        this._Router.navigate(['/home']);
+      }else{
+        this.error = response.message;
+      }
+    }) 
+  }
 }
